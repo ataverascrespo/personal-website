b7485b36929e8bbe1ae4f974ceac4a01750e0057
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-10-10 14:49:49 -0400

    Re-configure tailwind config

---
+++ tailwind.config.cjs
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        xs: '475px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
+    extend: {
+      keyframes: { 
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+        'slideInUp': {
+					"0%": { transform: ' translateY(100%)', opacity:0 },
+					"100%": { transform: 'translateY(0)', }
+        },
+        'zoomIn': {
+					"0%": { transform: 'scale(1.5, 1.5)', opacity: 0 },
+					"100%": { transform: 'scale(1, 1)'}
+				},
+        
+      },
+      animation: {
+        text: 'text 5s ease infinite',
+        slideInUp: "slideInUp 1.25s forwards",
+        zoomIn: "zoomIn 0.75s",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
