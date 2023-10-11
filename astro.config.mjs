264235961b70b2910a7862c83548deaaa5e95dde
Alex Taveras-Crespo <alexmanuelt@hotmail.com>
2023-10-11 10:59:04 -0400

    Configure site deploy path in astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alextaverascrespo.com',
   integrations: [
     tailwind({
     applyBaseStyles: false,
